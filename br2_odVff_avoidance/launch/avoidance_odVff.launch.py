d7fd77db9420e21ab7da6397e283271ea859b245
Andrés <andresegh0817@gmail.com>
2023-05-11 15:33:39 +0200

    Exercise 1 update.

---
+++ br2_odVff_avoidance/launch/avoidance_odVff.launch.py
@@ -0,0 +1,52 @@
+# Copyright 2021 Intelligent Robotics Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    params_file = os.path.join(
+        get_package_share_directory('br2_odVff_avoidance'),
+        'config',
+        'detector.yaml'
+        )
+
+    avoidance_odVff_cmd = Node(
+        package='br2_odVff_avoidance',
+        executable='avoidance_odVff',
+        parameters=[{
+          'use_sim_time': True
+        }, params_file],
+        remappings=[
+          ('input_image', '/head_front_camera/rgb/image_raw'),
+          ('joint_state', '/head_controller/state'),
+          ('joint_command', '/head_controller/joint_trajectory'),
+          ('input_scan', '/scan_raw'),
+          ('output_vel', '/nav_vel')
+        ],
+        output='screen'
+    )
+
+    ld = LaunchDescription()
+
+    # Add any actions
+    ld.add_action(avoidance_odVff_cmd)
+
+    return ld
